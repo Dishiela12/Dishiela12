62f525707f5b5fafff4253ea8c0fed168be18ef8
Dishiela ingrid M. Camunag <dishiela12@gmail.com>
2024-12-16 16:05:42 +0800

    Delete activity4.py

--- activity4.py
+++
@@ -1,4 +0,0 @@
-number1 = eval(input("Enter a number --->  "))
-number2 = eval(input("Enter a number --->  "))
-answer = number1 + number2
-print("the sum of " , number1 , " + " , number2 , " is " , answer)
